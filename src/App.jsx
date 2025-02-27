fb9592814a9189d8f078cb2001fd281ce026c1f9
Adele Zilinskaite <zilinskaite.adele@gmail.com>
2025-02-27 12:28:43 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import DashboardComponent from './Components/DashboardComponent/DashboardComponent';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Header /> */}
+      <div className="main-content">
+        <DashboardComponent />
+        {/* <Homepage /> */}
+      </div>
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
