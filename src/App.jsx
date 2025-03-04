db76f8a24d07fa68dc2c305d4e0e393a3f8b04cf
Adele Zilinskaite <zilinskaite.adele@gmail.com>
2025-03-04 11:25:57 +0100

    App.jsx update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import './App.css';
+import { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  // Routes,
+  // Route
+} from "react-router";
 import DashboardComponent from './Components/DashboardComponent/DashboardComponent';
 import HomePageComponent from './Components/HomePageComponent/HomePageComponent';
 import MainHeadingComponent from './Components/MainHeadingComponent/MainHeadingComponent'
 
 function App() {
+
+  useEffect(() => {
+    document.title = "Medicentral";
+  }, []);
 
   // useState to toggle dashboard using burger icon
   // const [isDashboardOpen, setIsDashboardOpen] = useState(false);
@@ -13,21 +23,37 @@ function App() {
   // };
 
   return (
+    <Router>
     <div className="App">
       {/* <Header /> */}
       {/* BURGER ICON IN HEADER SHOULD HAVE THIS:
        onToggleDashboard={toggleDashboard} */}
       <div className="main-content">
+      {/* <Routes> */}
+
+        {/* ROUTE LANDING PAGE */}
+        {/* ROUTE METHOD PAGE */}
+        {/* ROUTE LOGIN PAGE */}
+
+        {/* ROUTE HOMEPAGE */}
+        
         <DashboardComponent
         // isOpen={isDashboardOpen} onClose={toggleDashboard}
          />
         <div className="right-side">
         <MainHeadingComponent mainHeading="Hello, Doc" secondaryHeading="What are you looking for today?"/>
         <HomePageComponent />
+
+        {/* ROUTE PATIENT DATABASE */}
+
+
         </div>
+        {/* </Routes> */}
       </div>
+    
       {/* <Footer /> */}
     </div>
+    </Router>
   );
 }
 
