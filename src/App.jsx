c3a581e316a5c368947f3973dc6d6f57bf5f6822
Mandylaaane <ljungamanda@gmail.com>
2025-03-04 15:00:14 +0100

    PatientWidgets component started.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import PatientSearch from "./Components/PatientSearch/PatientSearch";
 import PatientHeader from "./Components/PatientHeader/PatientHeader";
+import PatientWidgets from "./Components/PatientWidgets/PatientWidgets";
 
 function App() {
   return (
     <div className="App">
       {/* <PatientSearch /> */}
-      <PatientHeader />
+      {/* <PatientHeader /> */}
+      <PatientWidgets />
     </div>
   );
 }
---
+++ src/Components/PatientWidgets/PatientWidgets.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./PatientWidgets.css";
+
+
+function PatientWidgets() {
+    return (
+
+    )
+}
+
+export default PatientWidgets;
